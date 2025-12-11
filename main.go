a45097e55e834b39c90406f8f9e25d3c72e32711
Serrel Vorne <serrelvorne@gmail.com>
2025-12-11 14:24:39 +0500

    NEW

--- main.go
+++ main.go
@@ -199,7 +199,7 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		log.Fatal("PORT environment variable is not set. Render requires this.")
 	}
 
 	// Создаем сервер с правильными настройками
